664d9baf3353eb5412ba46ed0c34a6beabcb75dc
Yana-Lesina <yanalesina3@gmail.com>
2021-10-11 18:25:31 +0300

    lesson06 + add

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
 'use strict'
 
+let ifRound = confirm('Хотите сыграть в игру?')
 
-function startGame(randomNum) {
+const game = function() {
+  
+  
+  function startRound(randomNum) {
+  alert('Загаданное число = ' + randomNum) 
+
+  let attempts = 10;
+
   const checkMatching = function() {
-    let userNum = prompt('Угадай число от 1 до 100: ')
+      let userNum = prompt('Угадай число от 1 до 100: ')
 
-    alert('вы ввели' + userNum + typeof userNum)
+      if (userNum !== null) { //обработка "отмены"
+          userNum = parseInt(userNum) //перевод в число: "", "   " и "впорыглодыол" => NaN
 
-    if (userNum !== null) { //обработка "отмены"
-        userNum = parseInt(userNum) //перевод в число: "", "   " и "впорыглодыол" => NaN
+          if( !isNaN(userNum)  ) {
 
-        if( !isNaN(userNum)  ) {
-          
-            if(userNum < randomNum) {
-              alert('Загаданное число больше' + userNum + typeof userNum)
-              checkMatching()
-            } else if (userNum > randomNum) {
-              alert('Загаданное число меньше' + userNum + typeof userNum)
-              checkMatching()
-            } else if (userNum == randomNum) {
-              alert('Поздравляю, Вы угадали!!!' + userNum + typeof userNum)
-            }
+              if (attempts > 0) {
 
-        } else {
-          confirm('Введи число!' + userNum + typeof userNum)
-          checkMatching()
-        }
+                if(userNum < randomNum) {
 
-    } else {
-      alert('Игра окончена' + userNum + typeof userNum)
-    }
- 
+                alert('Загаданное число больше, осталось попыток: ' + --attempts)
+
+                checkMatching()
+              } else if (userNum > randomNum) {
+                alert('Загаданное число меньше, осталось попыток: ' + --attempts)
+                checkMatching()
+              } else if (userNum == randomNum) {
+                ifRound = confirm('Поздравляю, Вы угадали!!!\nХотите сыграть ещё?')
+              }
+
+
+
+              } else {
+                ifRound = confirm('Попытки закончились, хотите сыграть еще?') 
+              }
+            
+          } else {
+            confirm('Введи число!')
+            checkMatching()
+          }
+
+      } else {
+        alert('Игра окончена')
+        ifRound = false
+      }
+  
   }
 
-  checkMatching()
+    checkMatching()
+  }
+
+
+  if(ifRound) {
+    startRound(Math.round(Math.random() * 99 + 1))
+      
+    } else { 
+      alert('Вы вышли из игры успешно!');
+      //break;
+    }
+
+
+  game()  
 }
 
-startGame(10)
+ 
+game()
