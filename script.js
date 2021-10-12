12d29f5df1ebd0f59f1c8d88671669e36b3728d3
Yana-Lesina <yanalesina3@gmail.com>
2021-10-12 19:21:25 +0300

    lesson06 corrected

--- script.js
+++ script.js
@@ -1,70 +1,39 @@
 'use strict'
 
-let ifRound = confirm('Хотите сыграть в игру?')
-
-const game = function() {
-  
-  
-  function startRound(randomNum) {
-  alert('Загаданное число = ' + randomNum) 
-
-  let attempts = 10;
-
-  const checkMatching = function() {
-      let userNum = prompt('Угадай число от 1 до 100: ')
-
-      if (userNum !== null) { //обработка "отмены"
-          userNum = parseInt(userNum) //перевод в число: "", "   " и "впорыглодыол" => NaN
-
-          if( !isNaN(userNum)  ) {
-
-              if (attempts > 0) {
-
-                if(userNum < randomNum) {
-
-                alert('Загаданное число больше, осталось попыток: ' + --attempts)
-
-                checkMatching()
-              } else if (userNum > randomNum) {
-                alert('Загаданное число меньше, осталось попыток: ' + --attempts)
-                checkMatching()
-              } else if (userNum == randomNum) {
-                ifRound = confirm('Поздравляю, Вы угадали!!!\nХотите сыграть ещё?')
-              }
-
-
-
-              } else {
-                ifRound = confirm('Попытки закончились, хотите сыграть еще?') 
-              }
-            
-          } else {
-            confirm('Введи число!')
-            checkMatching()
-          }
-
-      } else {
-        alert('Игра окончена')
-        ifRound = false
-      }
-  
-  }
-
-    checkMatching()
-  }
-
-
-  if(ifRound) {
-    startRound(Math.round(Math.random() * 99 + 1))
-      
-    } else { 
-      alert('Вы вышли из игры успешно!');
-      //break;
-    }
-
-
-  game()  
+const isNumber = function (num) {
+  return !isNaN(parseInt(num)) && isFinite(num) && num !== null;
 }
 
- 
-game()
+const game = function() {
+  const randomNum = Math.round(Math.random() * 99 + 1);
+  alert('Загаданное число = ' + randomNum); ////////////////////это проверка, потом удалить
+
+  return function repeat() {
+
+    let userNum = prompt('Угадай число от 1 до 100: ');
+
+    if( isNumber(userNum) ) {
+
+      if( userNum < randomNum ) {
+        alert ('Загаданное число больше')
+        repeat()
+      } else if ( userNum > randomNum ) {
+        alert ('Загаданное число меньше')
+        repeat()
+      } else {
+        alert ('Поздравляю, Вы угадали!!!')
+      }
+
+    } else {
+      if( userNum === null ) {
+        alert('Игра окончена')
+
+      } else {
+        alert ('Введи число!!!')
+        repeat ()
+      }
+    }
+  }
+}
+
+game()()
